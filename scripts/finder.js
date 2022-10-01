17eac89c9a15f329af1f067dbb4c0892a61ab070
Rahulaustralia <rahul090897pandey@gmail.com>
2022-10-02 01:56:18 +0530

    add day-5

---
+++ scripts/finder.js
@@ -0,0 +1,11 @@
+import { navbar } from "../components/navbar.js";
+
+let nav_div = document.getElementById("navbarRP");
+
+nav_div.innerHTML = navbar()
+
+import { footer } from "../components/footer.js";
+
+let foot_div = document.getElementById("RPfootermain");
+
+foot_div.innerHTML = footer();
