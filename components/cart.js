67da04bc18a6b3544359103aa225606f4d8d1ec2
Amit-Das <damit5221@gmail.com>
2022-10-02 12:39:45 +0530

    Updated

--- components/cart.js
+++ components/cart.js
@@ -6,7 +6,7 @@ const viewCart=()=>{
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdqmo9Tf03Ld28hxLG99nIdNbJLiC7QhJwRz1fNLvLAJknVxVmyXUbBrbvuUH1gfjNMTg&usqp=CAU" alt="">
           <p><span id="total_items">0</span>  ITEM</p>
         </div>
-        <a class="closebtn"> <img src="https://tampamagazines.com/wp-content/uploads/2020/06/black-x-png-1.png"></a>
+        <a id="closebtn"> <img src="https://tampamagazines.com/wp-content/uploads/2020/06/black-x-png-1.png"></a>
 
       </div>
       
@@ -20,7 +20,7 @@ const viewCart=()=>{
 </div> 
 
 
-
+z
 </div>`
 
 }
